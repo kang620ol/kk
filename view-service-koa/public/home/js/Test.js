dbe22a61f9833d9d87c442d410d1dca7fb25e693
kang620ol <76776078@qq.com>
2019-10-27 18:39:18 +0800

    up

---
+++ view-service-koa/public/home/js/Test.js
@@ -0,0 +1,12 @@
+
+export class Test {
+    defaultval = '789';
+
+    test() {
+        alert("调用Test类中test方法")
+    }
+}
+//var firstName = "jake";
+
+//export {firstName};
+
--- view-service-koa/public/home/js/hello.js
+++ view-service-koa/public/home/js/hello.js
@@ -1,7 +1,58 @@
-$("#jsonsubmit").click(function(){
+
+import {Test} from './Test.js';
+class Hello {
+    defaultval = '123';
+    
+    constructor(){
+        this.bindEvent();
+    }
+    
+    findUser() {
+        var test = new Test();
+        console.log(test.defaultval);
+        test.test();
+
+        var phone = $("#searchkey").val();
+        var url = "/getData";
+        phone = !phone ? this.defaultval : phone;
+        $.post(url,{phone:phone},function(res){
+            var arr = [1,2,3];
+            arr = arr.map(item => item + 1);
+            console.log("arr:"+arr);
+
+                if(res.length>0){
+                    alert("查询到"+res.length+"条结果:"+JSON.stringify(res));
+                   
+                }else{
+                    alert("没有查到该手机号相关信息");
+                }
+                console.log(res);
+            })
+      }
+
+      bindEvent() {
+        let _this = this;
+        $("#jsonsubmit").click(function(){
+            _this.findUser();
+        })
+      }
+
+}
+
+var hello = new Hello();
+//hello.findUser();
+
+
+
+
+/*$("#jsonsubmit").click(function(){
     var phone = $("#searchkey").val();
     var url = "/getData";
     $.post(url,{phone:phone},function(res){
+        var arr = [1,2,3];
+        arr = arr.map(item => item + 1);
+        console.log("arr:"+arr);
+
             if(res.length>0){
                 alert("查询到"+res.length+"条结果:"+JSON.stringify(res));
                
@@ -10,4 +61,4 @@ $("#jsonsubmit").click(function(){
             }
             console.log(res);
         })
-    })
+    })*/
